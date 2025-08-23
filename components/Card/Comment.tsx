4d6636bf622c6882c4b501f7570c91f2ddf869c5
맥동우 <t6u80o@naver.com>
2025-08-23 09:38:30 +0900

    design: 카드 디자인 변경

--- components/Card/Comment.tsx
+++ components/Card/Comment.tsx
@@ -20,11 +20,13 @@ export default function CommentCard({
 }: CommentCardProps) {
   return (
     <div className="w-full space-y-3 min-h-30 p-5 rounded-2xl border border-neutral-100 shadow">
-      <div className="flex gap-1 items-center">
-        <Nickname nickname={nickname} />
-        {isAuthor && <span className="text-xs text-blue-600">덕담 주인</span>}
+      <div>
+        <div className="flex gap-1 items-center">
+          <Nickname nickname={nickname} />
+          {isAuthor && <span className="text-xs text-blue-600">덕담 주인</span>}
+        </div>
+        <TimeStamp timeColor="gray" highlight time={createdAt} label="작성" />
       </div>
-      <TimeStamp timeColor="gray" highlight time={createdAt} label="작성" />
       <DashedHorizon />
       <ExpandablePayload canShow payload={payload} />
     </div>
